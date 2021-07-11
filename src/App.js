c3b57ca47f88f536766c2c8fbe982e925b8026e0
Lena Munad <munadlena@gmail.com>
2021-07-10 23:48:41 -0700

    Constructed our home page.

--- src/App.js
+++ src/App.js
@@ -14,10 +14,13 @@ import {
   VideoCameraOutlined,
 } from '@ant-design/icons';
 import { Row, Col } from 'antd';
+import { Card } from 'antd';
+import { Typography } from 'antd';
+import { white } from 'color-name';
 
 
 const { Header, Content, Footer, Sider } = Layout;
-
+const { Title } = Typography;
 
 function App() {
   return (
@@ -30,45 +33,79 @@ function App() {
         left: 0,
       }}
     >
+      
+      
       <div className="logo" />
       <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
         <Menu.Item key="1" icon={<UserOutlined />}>
-          nav 1
+          Hive Central
         </Menu.Item>
         <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-          nav 2
+          User Settings
         </Menu.Item>
         <Menu.Item key="3" icon={<UploadOutlined />}>
-          nav 3
+          Common Resources
         </Menu.Item>
         <Menu.Item key="4" icon={<BarChartOutlined />}>
-          nav 4
+          Public Video Chat
         </Menu.Item>
         <Menu.Item key="5" icon={<CloudOutlined />}>
-          nav 5
+          Main Chatrooms
         </Menu.Item>
-        <Menu.Item key="6" icon={<AppstoreOutlined />}>
-          nav 6
-        </Menu.Item>
-        <Menu.Item key="7" icon={<TeamOutlined />}>
-          nav 7
+        <Menu.Item key="6" icon={<TeamOutlined />}>
+          About Us
         </Menu.Item>
         <Menu.Item key="8" icon={<ShopOutlined />}>
-          nav 8
+          Friends
         </Menu.Item>
       </Menu>
     </Sider>
     <Layout className="site-layout" style={{ marginLeft: 200 }}>
-      <Header className="site-layout-background" style={{ padding: 0 }} />
+
+      <Header className="site-layout-background" style={{ padding: 0 }} >
+        <Row gutter={[16,16]} justify="center" align="middle">
+          <Col>
+              <Title level={3}
+            style={{
+              // textAlign: 'center',
+              paddingTop: '.5em',
+              color: 'white'
+            }} >Hive Central</Title>
+          </Col>
+        </Row>
+        </Header>
+
       <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
         <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
-        <Row>
-      <Col span={12}>col-12</Col>
-      <Col span={12}>col-12</Col>
+        <Row gutter={[48, 48]}>
+        <Col span={12}>
+        <Card title="Common Resources" bordered={false}>
+          Here is where you might find links and resources to many free bootcamps and courses. Most of these are online, which makes it even more comfortable and accesible!
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card title="Public Video Chat" bordered={false}>
+          This is where you might converse with your peers face-to-face, and experience more of a classroom setting. 
+        </Card>
+      </Col>
+
+      <Col span={12}> 
+      <Card title="Main Chatrooms" bordered={false}>
+          Come join the diverse live chat where you might find others who think-alike, and get in touch real time. You can even express yourself through emojis, but do remember to respect everyone here. 
+        </Card>
+      </Col>
+      <Col span={12}>
+      <Card title="About Us" bordered={false}>
+          We have a beautiful history, just as all humble things once begin. Our organziation is dedicated to educate everyone who is trying to get their feet wet in this competitive field of Computer Science.
+        </Card>
+      </Col>
         </Row>
+
+        
+
         </div>
       </Content>
-      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+      <Footer style={{ textAlign: 'center' }}>Coder's Hive ©2021 Created by Team Alpha</Footer>
     </Layout>
   </Layout>
   );
