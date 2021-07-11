0426c7dbd237df90f6ff50c76a4a07b0d54f42b9
Lena Munad <munadlena@gmail.com>
2021-07-10 17:41:40 -0700

    First comit: Adding react to our webpage

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import logo from './logo.svg';
+import 'antd/dist/antd.css';
+import './App.css';
+import {Button} from 'antd';
+import { Layout, Menu } from 'antd';
+import {
+  AppstoreOutlined,
+  BarChartOutlined,
+  CloudOutlined,
+  ShopOutlined,
+  TeamOutlined,
+  UserOutlined,
+  UploadOutlined,
+  VideoCameraOutlined,
+} from '@ant-design/icons';
+import { Row, Col } from 'antd';
+
+
+const { Header, Content, Footer, Sider } = Layout;
+
+
+function App() {
+  return (
+    <Layout>
+    <Sider
+      style={{
+        overflow: 'auto',
+        height: '100vh',
+        position: 'fixed',
+        left: 0,
+      }}
+    >
+      <div className="logo" />
+      <Menu theme="dark" mode="inline" defaultSelectedKeys={['4']}>
+        <Menu.Item key="1" icon={<UserOutlined />}>
+          nav 1
+        </Menu.Item>
+        <Menu.Item key="2" icon={<VideoCameraOutlined />}>
+          nav 2
+        </Menu.Item>
+        <Menu.Item key="3" icon={<UploadOutlined />}>
+          nav 3
+        </Menu.Item>
+        <Menu.Item key="4" icon={<BarChartOutlined />}>
+          nav 4
+        </Menu.Item>
+        <Menu.Item key="5" icon={<CloudOutlined />}>
+          nav 5
+        </Menu.Item>
+        <Menu.Item key="6" icon={<AppstoreOutlined />}>
+          nav 6
+        </Menu.Item>
+        <Menu.Item key="7" icon={<TeamOutlined />}>
+          nav 7
+        </Menu.Item>
+        <Menu.Item key="8" icon={<ShopOutlined />}>
+          nav 8
+        </Menu.Item>
+      </Menu>
+    </Sider>
+    <Layout className="site-layout" style={{ marginLeft: 200 }}>
+      <Header className="site-layout-background" style={{ padding: 0 }} />
+      <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
+        <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
+        <Row>
+      <Col span={12}>col-12</Col>
+      <Col span={12}>col-12</Col>
+        </Row>
+        </div>
+      </Content>
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
+  </Layout>
+  );
+}
+
+export default App;
