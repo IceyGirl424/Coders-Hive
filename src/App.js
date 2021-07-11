e7d4c81428ea910f60c0b744a36b11f249747d2b
Lena Munad <munadlena@gmail.com>
2021-07-11 01:33:10 -0700

    Stylized the homepage a bit, but still need to figure out images.

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import { Row, Col } from 'antd';
 import { Card } from 'antd';
 import { Typography } from 'antd';
 import { white } from 'color-name';
+import { Image } from 'antd';
 
 
 const { Header, Content, Footer, Sider } = Layout;
@@ -62,7 +63,7 @@ function App() {
     </Sider>
     <Layout className="site-layout" style={{ marginLeft: 200 }}>
 
-      <Header className="site-layout-background" style={{ padding: 0 }} >
+      <Header className="background-header" style={{ padding: 0 }} >
         <Row gutter={[16,16]} justify="center" align="middle">
           <Col>
               <Title level={3}
@@ -70,37 +71,44 @@ function App() {
               // textAlign: 'center',
               paddingTop: '.5em',
               color: 'white'
-            }} >Hive Central</Title>
+            }} > ≿————-❈ Hive Central ❈————-≾</Title>
           </Col>
         </Row>
         </Header>
+        
 
       <Content style={{ margin: '24px 16px 0', overflow: 'initial' }}>
         <div className="site-layout-background" style={{ padding: 24, textAlign: 'center' }}>
         <Row gutter={[48, 48]}>
         <Col span={12}>
-        <Card title="Common Resources" bordered={false}>
-          Here is where you might find links and resources to many free bootcamps and courses. Most of these are online, which makes it even more comfortable and accesible!
+        <Card title="»»————- Common Resources ————-««" bordered={false}>
+        ✧ Here is where you might find links and resources to many free bootcamps and courses. Most of these are online, which makes it even more comfortable and accesible! ✧
         </Card>
       </Col>
       <Col span={12}>
-        <Card title="Public Video Chat" bordered={false}>
-          This is where you might converse with your peers face-to-face, and experience more of a classroom setting. 
+        <Card title="»»————- Public Video Chat ————-««" bordered={false}>
+        ✧ This is where you might converse with your peers face-to-face, and experience more of a classroom setting. ✧
         </Card>
       </Col>
 
       <Col span={12}> 
-      <Card title="Main Chatrooms" bordered={false}>
-          Come join the diverse live chat where you might find others who think-alike, and get in touch real time. You can even express yourself through emojis, but do remember to respect everyone here. 
+      <Card title="»»————- Main Chatrooms ————-««" bordered={false}>
+      ✧ Come join the diverse live chat where you might find others who think-alike, and get in touch real time. You can even express yourself through emojis, but do remember to respect everyone here. ✧ 
         </Card>
       </Col>
       <Col span={12}>
-      <Card title="About Us" bordered={false}>
-          We have a beautiful history, just as all humble things once begin. Our organziation is dedicated to educate everyone who is trying to get their feet wet in this competitive field of Computer Science.
+      <Card title="»»————- About Us ————-««" bordered={false}>
+      ✧ We have a beautiful history, just as all humble things once begin. Our organziation is dedicated to educate everyone who is trying to get their feet wet in this competitive field of Computer Science. ✧
         </Card>
       </Col>
         </Row>
 
+        {/* This is a comment: 
+        <Image
+        width={200}
+        src="C:\Users\Alina\ReactProjects\coders-hive\public\Coders_Hive_1.PNG"
+        />  
+        */}
         
 
         </div>
@@ -110,5 +118,7 @@ function App() {
   </Layout>
   );
 }
+
+
 
 export default App;
